3a567fbd6c691bb51e255dc2ae6a17c9f2caa065
Valentino Ricci <contactoricci@gmail.com>
2025-02-12 10:08:30 -0300

    Ex 3

--- Engineering-Practice/1-riccirpb-crud.js
+++ Engineering-Practice/1-riccirpb-crud.js
@@ -2,7 +2,7 @@ class User {
     constructor(username, age, password, createdAt = null) {
         this.username = username;
         this.age = age;
-        this.createdAt = createdAt ? new Date(createdAt): new Date();
         this.password = password;
+        this.createdAt = createdAt ? new Date(createdAt): new Date();
     }
   }
---
+++ Engineering-Practice/3-riccirpb-crud.js
@@ -0,0 +1,1 @@
+let arrayOfUsers = [{username: "Riccirpb", age: 25, password: "Radio123", createdAt: "2025-02-12T15:30:45.678Z"},{username: "Agus2", age: 30, password: "12345678", createdAt: "2024-12-15T22:55:45.678Z"},{username: "Arielito", age: 27, password: "susanaDiosa", createdAt: "2003-07-28T18:10:45.655Z"}]
